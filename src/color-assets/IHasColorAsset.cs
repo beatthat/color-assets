7a4ae0be35fbdcf3ff994e648159ef0c8b18c62e
larrykirschner <larrykirschner@gmail.com>
2018-02-19 21:43:06 -0800

    ColorAssets now update all scene user objects when the asset is update in editor

---
+++ src/color-assets/IHasColorAsset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeatThat
+{
+	public interface IHasColorAsset  {
+
+		ColorAsset colorAsset { get; }
+
+		void OnColorAssetUpdated (ColorAsset colorAsset);
+	}
+}
